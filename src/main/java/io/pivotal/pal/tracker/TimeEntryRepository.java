c4fc25ec8a902dc7c3683ceed3212e0deb2b2558
sumanvarma <sumanvarma83@gmail.com>
2020-01-28 21:30:17 -0800

    Spring MVC version3

--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -2,7 +2,7 @@ package io.pivotal.pal.tracker;
 
 import java.util.List;
 
-interface TimeEntryRepository{
+public interface TimeEntryRepository{
     public TimeEntry create(TimeEntry timeEntry);
     public TimeEntry find(Long id);
     public List<TimeEntry> list();
